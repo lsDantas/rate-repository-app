eaaaab347e3f32ce1e95d3fc2472b966b1110418
Leo <leonardosdantas@protonmail.com>
2021-09-23 20:37:29 -0300

    Exercise 10.14

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -7,6 +7,7 @@ import * as yup from 'yup';
 import FormikTextInput from './FormikTextInput';
 
 import theme from '../theme';
+
 import useSignIn from './hooks/useSignIn';
 
 const initialValues = {
@@ -78,9 +79,10 @@ const SignIn = () => {
     const { username, password } = values;
     try {
       const { data } = await signIn({ username, password });
-      console.log('Post Mutation', data);
+      const receivedToken = data.authorize.accessToken;
+      console.log(data);
     } catch (e) {
-      console.log('Submit Error: ', e);
+      console.log(e);
     }
   };
 
