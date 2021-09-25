ef3a440ba2665a766b53075fd87d2d2db9394b6f
Leo <leonardosdantas@protonmail.com>
2021-09-24 23:58:41 -0300

    Working on Exercise 10.23

--- src/components/RepositoryList/index.jsx
+++ src/components/RepositoryList/index.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-native';
 import { FlatList, View, SafeAreaView, StyleSheet, Pressable } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 
 import useRepositories from '../../hooks/useRepositories';
 
@@ -18,11 +19,35 @@ const styles = StyleSheet.create({
     height: 10,
     backgroundColor: '#c5c5c5',
   },
+  pickerContainer: {
+    flex: 1,
+    flexShrink: 0,
+    height: 60,
+    padding: 20,
+    backgroundColor: '#c5c5c5'
+  }
 });
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
-export const RepositoryListContainer = ({ repositories }) => {
+const RepositoryOrderSelector = ({ selectedOrder, setSelectedOrder }) => {
+  return (
+    <View style={styles.pickerContainer} >
+      <Picker
+        selectedValue={selectedOrder}
+        onValueChange={(itemValue) =>
+          setSelectedOrder(itemValue)
+        }
+      >
+        <Picker.Item label='Latest Repositories' value='latest' />
+        <Picker.Item label='Highest rated repositories' value='highest' />
+        <Picker.Item label='Lowest rated repositories' value='lowest' />
+      </Picker>
+    </View>
+  );
+};
+
+export const RepositoryListContainer = ({ repositories, selectedOrder, setSelectedOrder}) => {
   const history = useHistory();
   const repositoryNodes = repositories
     ? repositories.edges.map((edge) => edge.node)
@@ -48,15 +73,17 @@ export const RepositoryListContainer = ({ repositories }) => {
         renderItem={renderItem}
         style={{ flex: 1 }}
         testID='repository-list-container'
+        ListHeaderComponent={<RepositoryOrderSelector selectedOrder={selectedOrder} setSelectedOrder={setSelectedOrder} />}
       />
     </SafeAreaView>
   );
 };
 
 const RepositoryList = () => {
+  const [selectedOrder, setSelectedOrder] = useState();
   const { repositories } = useRepositories();
 
-  return <RepositoryListContainer repositories={repositories} />;
+  return <RepositoryListContainer repositories={repositories} selectedOrder={selectedOrder} setSelectedOrder={setSelectedOrder} />;
 };
 
 export default RepositoryList;
