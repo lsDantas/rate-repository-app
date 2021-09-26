e08bf735b514613a3e249041dda86bb8a7d7f33a
Leo <leonardosdantas@protonmail.com>
2021-09-26 00:38:16 -0300

    Small fix.

--- src/components/MyReviewsView/index.js
+++ src/components/MyReviewsView/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { FlatList, StyleSheet, View, Text } from 'react-native';
+import { FlatList, StyleSheet, View } from 'react-native';
 import ReviewItem from '../SingleRepositoryView/ReviewItem';
 
 import useVerifyUser from '../../hooks/useVerifyUser';
